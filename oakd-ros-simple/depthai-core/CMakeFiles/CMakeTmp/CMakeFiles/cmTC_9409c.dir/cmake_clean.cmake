04fea1cd71d0a2624c8728a44179ea120f746153
sanghun17 <sanghun17@unist.ac.kr>
2025-01-25 00:41:49 +0900

    initial commit

---
+++ oakd-ros-simple/depthai-core/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_9409c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_9409c.dir/testCCompiler.c.o"
+  "cmTC_9409c"
+  "cmTC_9409c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_9409c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ oakd-ros-simple/depthai-core/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_9409c.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_9409c.
+# This may be replaced when dependencies are built.
